e5b6f14bfcc61120ed635f696e9afb2129ba3ecd
sabbir10s <sabbirahmed1023@gmail.com>
2022-05-23 19:31:07 +0600

    for heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
-require('dotenv').config()
-
-const port = process.env.PROT || 5000;
-
+const express = require('express')
 const cors = require('cors');
-app.use(cors())
-app.use(express.json())
+
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqqeord.mongodb.net/?retryWrites=true&w=majority`;
