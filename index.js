c239e1efa261c8f9105bbed0a17dd8a82c8bb5ee
sabbir10s <sabbirahmed1023@gmail.com>
2022-05-23 19:10:43 +0600

    DB connection done

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iqqeord.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect()
+        console.log("NEXIQ Connected");
+        const productCollection = client.db('NEXIQ').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send("Running the NEXIQ Server");
 })
