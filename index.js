0ef424dcee6543d4758a18e04d1e85480d2252db
sabbir10s <sabbirahmed1023@gmail.com>
2022-06-03 21:41:17 +0600

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 
