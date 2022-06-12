4ef815bc3e9989867da1506f501af5911d425f97
sabbir10s <sabbirahmed1023@gmail.com>
2022-06-12 13:36:05 +0600

    user update

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function run() {
 
 
         // check user admin or not 
-        app.get('/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
